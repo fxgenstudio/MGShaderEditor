c625aa82d8b08f0d3b4efe6c374b18bafd820845
johann <jnadalutti@hotmail.com>
2015-09-17 21:21:37 +0200

    added files

---
+++ Src/Tools/MGShaderEditor/MGShaderEditor/TextureSlotsUserControl.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MGShaderEditor
+{
+  partial class TextureSlotsUserControl
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // TextureSlotsUserControl
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.DoubleBuffered = true;
+      this.Name = "TextureSlotsUserControl";
+      this.Size = new System.Drawing.Size(299, 449);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
---
+++ Src/Tools/MGShaderEditor/MGShaderEditor/TextureSlotsUserControl.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using System.Diagnostics;
+
+namespace MGShaderEditor
+{
+  /// <summary>
+  /// UserControl for Textures Slots display and selection
+  /// </summary>
+  public partial class TextureSlotsUserControl : UserControl
+  {
+    public const int SlotsCount = 4;
+
+    #region -- Fields --
+    Image []m_imagesSlots = new Image[SlotsCount];
+    #endregion
+
+    #region -- Properties --
+    public Game1 Game1 { get; set; }
+    #endregion
+
+    #region -- ctor --
+    public TextureSlotsUserControl()
+    {
+      InitializeComponent();
+    }
+    #endregion
+
+    #region -- Methods --
+    public void SetTextureSlot(int _nSlotIdx, string _strFileName)
+    {
+      var streamT = File.OpenRead(_strFileName);
+      Texture2D tex01 = Texture2D.FromStream(Game1.GraphicsDevice, streamT);
+      streamT.Close();
+
+      SetTextureSlot(_nSlotIdx, tex01);
+    }
+    public void SetTextureSlot(int _nSlotIdx, Texture2D _tex)
+    {
+      if (_nSlotIdx < 0 || _nSlotIdx >= SlotsCount)
+        return;
+
+      MemoryStream mem = new MemoryStream();
+
+      _tex.SaveAsPng(mem, 256, 256);
+
+      m_imagesSlots[_nSlotIdx] = Image.FromStream(mem);
+
+      Game1.SetTextureSlot(_nSlotIdx, _tex);
+
+      Refresh();
+    }
+    #endregion
+
+    #region -- UI Events --
+    protected override void OnSizeChanged(EventArgs e)
+    {
+      base.OnSizeChanged(e);
+
+      AutoScrollMinSize = new Size(0, Width * SlotsCount);
+    }
+    protected override void OnDoubleClick(EventArgs e)
+    {
+      //Get slot idx
+      MouseEventArgs evt = e as MouseEventArgs;
+      int nSlotIdx = (evt.Y - AutoScrollPosition.Y )/ ClientSize.Width;
+      if (nSlotIdx < 0 || nSlotIdx >= SlotsCount)
+        return;
+
+      //Show FileDialog
+      OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+      openFileDialog1.Filter = "bmp files (*.bmp)|*.bmp|png files (*.png)|*.png|jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+      openFileDialog1.RestoreDirectory = true;
+
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        try
+        {
+          SetTextureSlot(nSlotIdx, openFileDialog1.FileName);
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+        }
+      }
+
+    }
+    protected override void OnScroll(ScrollEventArgs se)
+    {
+      base.OnScroll(se);
+
+      Refresh();
+    }
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      base.OnPaint(e);
+
+      int w = ClientSize.Width;
+      int h = ClientSize.Height;
+
+      e.Graphics.FillRectangle(Brushes.Black, 0,0, w,h);
+
+      //Translate matrix to scrollbar offset
+      e.Graphics.TranslateTransform(0, AutoScrollPosition.Y);
+
+      //Draw texture slots
+      Rectangle rc = new Rectangle(0,0, w, w);
+      SolidBrush brSlotBk = new SolidBrush(Color.Gray);
+
+      rc.Inflate(-4, -4);//For Borders
+
+      for (int i = 0; i < SlotsCount; i++)
+      {
+        //Slot backgroud
+        e.Graphics.FillRectangle(brSlotBk, rc);
+
+        //Texture image
+        var img = m_imagesSlots[i];
+
+        if (img != null)
+          e.Graphics.DrawImage(img, rc);
+
+        //Display Slot Name
+        if (img != null)
+          e.Graphics.DrawString(string.Format("Texture Slot {0} {1}x{2}", i, img.Width, img.Height), SystemFonts.DefaultFont, Brushes.White, rc);
+        else
+          e.Graphics.DrawString(string.Format("Texture Slot {0} ", i), SystemFonts.DefaultFont, Brushes.White, rc);
+
+        rc.Offset(0,w);
+      }
+
+
+
+
+    }
+    #endregion
+
+  }
+}
