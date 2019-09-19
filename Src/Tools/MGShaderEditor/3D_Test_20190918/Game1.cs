d00052013d7282d79ee613c6166334a4173027d0
Josiah T Avery <josiah.t.avery.ctr@mail.mil>
2019-09-18 20:54:49 -0400

    Getting into it, with Monogame 3D :)

---
+++ Src/Tools/MGShaderEditor/3D_Test_20190918/Game1.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace _3D_Test_20190918
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        VertexBuffer vertexBuffer;
+
+        BasicEffect basicEffect;
+        Matrix world = Matrix.CreateTranslation(0, 0, 0);
+        Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 3), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.01f, 100f);
+
+        KeyboardState CurrentKeyState, PreviousKeyState;
+        private double zOffset;
+        private float xCamPosition;
+        private float yCamPosition;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            basicEffect = new BasicEffect(GraphicsDevice);
+
+            VertexPositionColor[] vertices = new VertexPositionColor[3];
+            vertices[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
+            vertices[1] = new VertexPositionColor(new Vector3(+0.5f, 0, 0), Color.Red);
+            vertices[2] = new VertexPositionColor(new Vector3(-0.5f, 0, 0), Color.Red);
+
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(vertices);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            PreviousKeyState = CurrentKeyState;
+            CurrentKeyState = Keyboard.GetState();
+
+            // Move camera ( look at)
+            view = Matrix.CreateLookAt(new Vector3(xCamPosition, yCamPosition, Math.Abs((float)(20.0f * Math.Sin(zOffset++ * Math.PI / 280)))),
+                                       new Vector3(0, 0, 0),
+                                       new Vector3(0, 1, 0));
+
+            if (CurrentKeyState[Keys.Left] == KeyState.Down)
+                xCamPosition += 0.125f;
+            if (CurrentKeyState[Keys.Right] == KeyState.Down)
+                xCamPosition -= 0.125f;
+            if (CurrentKeyState[Keys.Up] == KeyState.Down)
+                yCamPosition += 0.125f;
+            if (CurrentKeyState[Keys.Down] == KeyState.Down)
+                yCamPosition -= 0.125f;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+            
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            basicEffect.VertexColorEnabled = false;
+
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.FillMode = FillMode.Solid;
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
+
+            basicEffect.VertexColorEnabled = true;
+
+            rasterizerState.FillMode = FillMode.WireFrame;
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
+
+
+
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
